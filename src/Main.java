438512cc1f302af3e0556c7f356ef5071e6bd4b4
Aliaksandr Hei <xander1006@gmail.com>
2020-12-16 21:42:53 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,46 @@ public class Main {
         System.out.println("Enter random limit numeral M:");
         int m = scan.nextInt();
 
-        Random random = new Random();           //инициализация класса Random
+        //инициализация класса Random
+        Random random = new Random();
 
-        int [][] matrix = new int[n][n];        //инициализация матрицы
-        for (int i= 0; i < n; i++){             //заполнение матрицы
+        //инициализация матрицы
+        int [][] matrix = new int[n][n];
+        //заполнение матрицы
+        for (int i= 0; i < n; i++){
             for (int j = 0; j < n; j++){
                 matrix[i][j] = random.nextInt(m*2+1)-m; // генерация случайных чисел
                 System.out.print(matrix[i][j]+" ");
             }
             System.out.println();
         }
+        //Вводим столбец матрицы для сортировки
+        System.out.println("Enter sorting column: ");
+        int k = scan.nextInt();
+        //сортировка матрицы
+        boolean bool = false;
+        int temp;
+        while (!bool){
+            bool = true;
+        for (int i = 0; i < n-1; i++) {
+            if (matrix[i][k-1] > matrix[i + 1][k-1]){
+                bool = false;
+                for (int j = 0; j < n; j++) {
+                    temp = matrix[i][j];
+                    matrix[i][j] = matrix[i + 1][j];
+                    matrix[i + 1][j] = temp;
+                }
+            }
+        }
+        }
+            //Вывод отсортированной матрицы
+            for (int i= 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    System.out.print(matrix[i][j]+" ");
+                }
+                System.out.println();
+            }
+
+        }
     }
-}
+
